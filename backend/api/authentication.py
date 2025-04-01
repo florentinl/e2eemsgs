50f4d7867893df96c7728cee4e2a53de5262eaad
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2025-04-01 09:46:33 +0200

    chore: format file with ruff

--- backend/api/authentication.py
+++ backend/api/authentication.py
@@ -15,15 +15,16 @@ def generate_random_string(n: int) -> str:
     symbols = ascii_letters + digits
     return "".join(choice(symbols) for _ in range(n))
 
+
 class ChallengeRequest(BaseModel):
     username: str
 
 
 @router.post("/auth/signup")
-def signup(user: User, response : Response) -> User:
+def signup(user: User, response: Response) -> User:
     with Session(engine) as session:
         session.add(user)
-        try :
+        try:
             session.commit()
         except exc.IntegrityError:
             response.status_code = 409
@@ -34,40 +35,44 @@ def signup(user: User, response : Response) -> User:
         session.refresh(user)
         return user
 
+
 @router.post("/auth/login_challenge")
-def challenge(req: ChallengeRequest, response : Response) -> Challenge:
+def challenge(req: ChallengeRequest, response: Response) -> Challenge:
     with Session(engine) as session:
         # Getting user key
-        try :
+        try:
             user = session.exec(select(User).where(User.username == req.username)).one()
-        except :
+        except:
             response.status_code = 500
-            return Challenge(username="",challenge="")
-        
+            return Challenge(username="", challenge="")
+
         # Generating challenge
-        chall = Challenge(username=req.username,challenge=generate_random_string(32))
+        chall = Challenge(username=req.username, challenge=generate_random_string(32))
         session.add(chall)
         try:
             session.commit()
         except:
             response.status_code = 500
-            return Challenge(username="",challenge=chall.challenge)
+            return Challenge(username="", challenge=chall.challenge)
         session.refresh(chall)
-        
+
         # Encrypt challenge with user's key
         chall.challenge = asym_encrypt(chall.challenge, user.public_key)
         return chall
-    
+
+
 @router.post("/auth/login_answer")
-def answer(answer: Challenge, response : Response) -> Challenge:
+def answer(answer: Challenge, response: Response) -> Challenge:
     with Session(engine) as session:
         # Getting challenge
-        try :
-            chall = session.exec(select(Challenge).where(Challenge.id == answer.id)).one()
-        except :
+        try:
+            chall = session.exec(
+                select(Challenge).where(Challenge.id == answer.id)
+            ).one()
+        except:
             response.status_code = 500
-            return Challenge(id=0,username="",challenge="")
-        
+            return Challenge(id=0, username="", challenge="")
+
         # Comparing answers
         if answer.challenge == chall.challenge:
             return chall
