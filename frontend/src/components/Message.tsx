950cbc2d092f6f727828f6955c05b36e8d6508f2
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2025-04-07 21:20:51 +0200

    show sent messages differently

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,23 +1,30 @@
 import { Card, CardContent, Typography } from "@mui/material";
 import type { Message } from "../types";
+import type { User } from "../api-client";
 
 export type MessageProps = {
   msg: Message;
+  self: User;
 };
 
-const MessageDisplay = ({ msg }: MessageProps) => {
+const MessageDisplay = ({ msg, self }: MessageProps) => {
   return (
     <Card
       sx={{
         my: 1,
         maxWidth: "75%",
         width: "fit-content",
+        ...(self.username === msg.sender_name
+          ? { backgroundColor: "rgb(135,117,218)" }
+          : {}),
       }}
     >
       <CardContent>
-        <Typography variant="subtitle2" color="textSecondary">
-          {msg.sender_name}
-        </Typography>
+        {self.username !== msg.sender_name && (
+          <Typography variant="subtitle2" color="textSecondary">
+            {msg.sender_name}
+          </Typography>
+        )}
         <Typography variant="body1" sx={{ whiteSpace: "pre-line" }}>
           {msg.content}
         </Typography>
