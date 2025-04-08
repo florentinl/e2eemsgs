384bc17fca2b788b7534a48a842e674ee8239d91
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2025-04-08 11:38:01 +0200

    UserProfileDialog: display user social link as well

--- frontend/src/components/UserProfileDialog.tsx
+++ frontend/src/components/UserProfileDialog.tsx
@@ -4,12 +4,13 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import { MenuItem, Typography } from "@mui/material";
+import { Box, IconButton, MenuItem, Typography } from "@mui/material";
+import { Close, Link } from "@mui/icons-material";
 
 type UserProfileDialogProps = {
   username: string | null;
-  description: string | null | undefined;
-  socialLink: string | null | undefined;
+  description?: string;
+  socialLink?: string;
 };
 
 export default function UserProfileDialog({
@@ -27,16 +28,47 @@ export default function UserProfileDialog({
   return (
     <React.Fragment>
       <MenuItem onClick={handleOpen}>View Profile</MenuItem>
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>{username}'s profile</DialogTitle>
-        <DialogContent>
-          <Typography>{description}</Typography>
-          <Typography>{socialLink}</Typography>
+      <Dialog open={open} onClose={handleClose} maxWidth="xs" fullWidth>
+        <DialogTitle
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box display={"flex"} gap={"1em"}>
+            {username}
+            {socialLink && (
+              <a href={socialLink}>
+                <Link />
+              </a>
+            )}
+          </Box>
+          <IconButton onClick={handleClose}>
+            <Close />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography variant="body1" sx={{ mb: 2 }}>
+            {description}
+          </Typography>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Close</Button>
         </DialogActions>
       </Dialog>
+      {/* <Dialog open={open} onClose={handleClose} maxWidth="xs" fullWidth>
+        <DialogTitle align="center">
+          <Typography variant="h4">{username}</Typography>
+        </DialogTitle>
+        <DialogContent>
+          <Typography variant="caption">Bio</Typography>
+          <Typography>{description}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog> */}
     </React.Fragment>
   );
 }
